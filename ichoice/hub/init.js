d8c4ade75d71856607a3ee51ef72b90eb4886fea
chaosforfun <chaos.forfun@gmail.com>
2016-12-28 11:33:25 +0800

    fix: online restart

--- ichoice/hub/init.js
+++ ichoice/hub/init.js
@@ -17,10 +17,10 @@ let EventSource = require('eventsource');
 let devices = [];
 devices.push(require('../devices/xtxy'))
 devices.push(require('../devices/shouhuan'))
-//devices.push(require('../devices/weight'))
-//devices.push(require('../devices/bbt'))
-//devices.push(require('../devices/bp'))
-//devices.push(require('../devices/ecg'))
+devices.push(require('../devices/weight'))
+devices.push(require('../devices/bbt'))
+devices.push(require('../devices/bp'))
+devices.push(require('../devices/ecg'))
 
  //devices.push(require('../devices/xindian'));
  devices.push(require('../devices/xueya'));
